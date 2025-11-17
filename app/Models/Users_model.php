667755168e24290e2e7a92b3ba75e04cb1ba2b21
Marlo22 <veluzmarlo959@gmail.com>
2025-11-17 14:41:40 +0800

    feat: Create Users_model for user data management

---
+++ app/Models/Users_model.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class Users_model extends Model {
+    protected $table      = 'tblusers';
+
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+
+    protected $returnType     = 'array';
+
+    protected $allowedFields = [
+        'profile_pic',
+        'username',
+        'password',
+        'fullname',
+        'email',
+        'datecreated',
+        'isverified',
+        'token'
+    ];
+
+    protected bool $allowEmptyInserts = false;
+}
+?>
