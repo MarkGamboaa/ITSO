66bddce4e75455d1ec7672251419429f186468ba
Marlo22 <veluzmarlo959@gmail.com>
2025-11-21 17:03:09 +0800

    feat: update user details view to display dynamic user information

--- app/Views/ITSO/users/users_view_view.php
+++ app/Views/ITSO/users/users_view_view.php
@@ -3,10 +3,11 @@
 
         <h2>User Details</h2>
         <dl class="row">
-            <dt class="col-sm-3">ID</dt><dd class="col-sm-9">1</dd>
-            <dt class="col-sm-3">Name</dt><dd class="col-sm-9">Sample Name</dd>
-            <dt class="col-sm-3">Email</dt><dd class="col-sm-9">sample@feutech.edu.ph</dd>
-            <dt class="col-sm-3">Role</dt><dd class="col-sm-9">Associate</dd>
+            <dt class="col-sm-3">ID</dt><dd class="col-sm-9" id="user_id"><?= $user['user_id']?></dd>
+            <dt class="col-sm-3">First Name</dt><dd class="col-sm-9" id="first_name"><?= $user['first_name']?></dd>
+            <dt class="col-sm-3">Last Name</dt><dd class="col-sm-9" id="last_name"><?= $user['last_name']?></dd>
+            <dt class="col-sm-3">Email</dt><dd class="col-sm-9" id="email"> <?= $user['email']?></dd>
+            <dt class="col-sm-3">Role</dt><dd class="col-sm-9" id="role"><?= $user['role']?></dd>
         </dl>
         <a href="<?= base_url('users') ?>" class="btn btn-secondary mb-3">&larr; Back</a>
     </div>
