2cec0ab71bc99144d8cccab1db98184ed1d7b5cd
Marlo22 <veluzmarlo959@gmail.com>
2025-11-21 23:03:51 +0800

    fix: correct class name from ReservationsModel to Reservations_Model for consistency

--- app/Models/Reservations_model.php
+++ app/Models/Reservations_model.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class ReservationsModel extends Model
+class Reservations_Model extends Model
 {
     protected $table            = 'reservations';
     protected $primaryKey       = 'reservation_id';
