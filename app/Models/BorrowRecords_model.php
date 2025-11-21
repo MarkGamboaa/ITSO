e59b504726c0dd69269cee4a6b4b6f4c99675de4
Marlo22 <veluzmarlo959@gmail.com>
2025-11-21 23:03:39 +0800

    fix: correct class name from BorrowRecordModel to BorrowRecords_Model for consistency

--- app/Models/BorrowRecords_model.php
+++ app/Models/BorrowRecords_model.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class BorrowRecordModel extends Model
+class BorrowRecords_Model extends Model
 {
     protected $table            = 'borrow_records';
     protected $primaryKey       = 'id';
