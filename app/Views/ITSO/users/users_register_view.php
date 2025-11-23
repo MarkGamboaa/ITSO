20bcf046131778d8ed0f8f796f7141e2abb251c4
Marlo22 <veluzmarlo959@gmail.com>
2025-11-23 19:19:05 +0800

    refactor: update user registration form to remove 'ITSO Personnel' role option

--- app/Views/ITSO/users/users_register_view.php
+++ app/Views/ITSO/users/users_register_view.php
@@ -16,7 +16,7 @@
             <div class="mb-3"><label class="form-label">Last Name</label><input class="form-control" name="last_name" id="last_name" /></div>
             <div class="mb-3"><label class="form-label">Email</label><input type="email" class="form-control" name="email" id="email" /></div>
             <div class="mb-3"><label class="form-label">Role</label>
-                <select class="form-select" name="role" id="role"><option>ITSO Personnel</option><option>Associate</option><option>Student</option></select>
+                <select class="form-select" name="role" id="role"><option>Associate</option><option>Student</option></select>
             </div>
             <div class="mt-3 d-flex justify-content-start">
                 <a href="<?= base_url('users') ?>" class="btn btn-secondary me-2">&larr; Back</a>
