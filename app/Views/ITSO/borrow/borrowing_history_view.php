ec640f5ceefa16bfe6369fe727ab85dd314027a4
Marlo22 <veluzmarlo959@gmail.com>
2025-11-23 19:20:02 +0800

    feat: update borrowing history view to display user ID, accessories, and quantity

--- app/Views/ITSO/borrow/borrowing_history_view.php
+++ app/Views/ITSO/borrow/borrowing_history_view.php
@@ -4,22 +4,32 @@
         <table class="table">
             <thead>
                 <tr>
-                    <th>Ref</th>
+                    <th>User ID</th>
                     <th>User</th>
                     <th>Item</th>
+                    <th>Accessories</th>
+                    <th>Quantity</th>
                     <th>Borrowed</th>
                     <th>Returned</th>
                 </tr>
             </thead>
             <tbody>
+                <?php foreach ($borrowing as $borrow): ?>
                 <tr>
-                    <td>BR-001</td>
-                    <td>Marlon Viluz</td>
-                    <td>Laptop</td>
-                    <td>2025-10-10</td>
-                    <td>2025-10-12</td>
+                    <td><?= $borrow['user_id'] ?></td>
+                    <td><?= $borrow['user_name'] ?></td>
+                    <td><?= $borrow['equipment_name'] ?></td>
+                    <td><?= $borrow['accessories'] ?></td>
+                    <td><?= $borrow['borrow_quantity'] ?></td>
+                    <td><?= date('Y-m-d', strtotime($borrow['borrowed_at'])) ?></td>
+                    <td><?= date('Y-m-d', strtotime($borrow['returned_at'])) ?></td>
                 </tr>
+                <?php endforeach; ?>
             </tbody>
         </table>
+        <?= $pager->links(); ?>
+        <div class="mt-3 d-flex justify-content-start">
+                <a href="<?= base_url('borrowing') ?>" class="btn btn-secondary me-2">&larr; Back</a>
+        </div>
     </div>
 </main>
