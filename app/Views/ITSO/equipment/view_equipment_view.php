19b06762c52b809fb294e81dbfab2eb5e282421a
carlmarinyo <marinojerome2@gmail.com>
2025-11-24 23:29:26 +0800

    refactor: change hard coded view into dynamic

--- app/Views/ITSO/equipment/view_equipment_view.php
+++ app/Views/ITSO/equipment/view_equipment_view.php
@@ -1,16 +1,29 @@
 <main>
     <div class="itso-container">
         <h2>Equipment Details</h2>
-        <p>Equipment ID: <?= isset($id) ? esc($id) : 'EQ-001' ?></p>
+
+        <p>
+            Equipment ID: <?= esc($equipment['equipment_id']) ?>
+        </p>
+
         <dl class="row">
-            <dt class="col-sm-3">Name</dt><dd class="col-sm-9">Laptop (with charger)</dd>
-            <dt class="col-sm-3">Count</dt><dd class="col-sm-9">5</dd>
-            <dt class="col-sm-3">Accessories</dt><dd class="col-sm-9">Charger</dd>
+            <dt class="col-sm-3">Name</dt>
+            <dd class="col-sm-9"><?= esc($equipment['name']) ?></dd>
+
+            <dt class="col-sm-3">Total Count</dt>
+            <dd class="col-sm-9"><?= esc($equipment['total_count']) ?></dd>
+
+            <dt class="col-sm-3">Available</dt>
+            <dd class="col-sm-9"><?= esc($equipment['available_count']) ?></dd>
+
+            <dt class="col-sm-3">Accessories</dt>
+            <dd class="col-sm-9">
+                <?= $equipment['accessories'] ? esc($equipment['accessories']) : 'None' ?>
+            </dd>
         </dl>
+
         <div class="mt-3 d-flex justify-content-start">
-            <a href="<?= base_url('equipment') ?>" class="btn btn-secondary">
-                <i class="bi bi-arrow-left"></i> Back
-            </a>
+            <a href="<?= base_url('equipment') ?>" class="btn btn-secondary">&larr; Back</a>
         </div>
     </div>
 </main>
